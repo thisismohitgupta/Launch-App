5102effa45ceae3c6229b3c8ab63ec280b63cd37
anurag-coinshred <anurag@coinshred.com>
2021-12-27 14:54:39 +0530

    update

--- JS/location.js
+++ JS/location.js
@@ -12,7 +12,7 @@ var settingsLoc = {
 
 
 
-$.ajax(settingsLoc).done(function (response) {
+$.ajax(settingsLoc).done(function (jsondata) {
   console.log(jsondata)
   price = jsondata.geoplugin_currencyConverter, currencyCode = jsondata.geoplugin_currencyCode, city = jsondata.geoplugin_city, country = jsondata.geoplugin_countryName, timezone = geoplugin_timezone;
 

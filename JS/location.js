671ec4f655877dadde707bd3213c3476dd17512e
anurag-coinshred <anurag@coinshred.com>
2021-12-27 16:45:59 +0530

    update

--- JS/location.js
+++ JS/location.js
@@ -1,33 +1,28 @@
 
 // code for getting the ip address of user
-var ip;
-$.getJSON("https://api.ipify.org?format=json", function(data) {
-       ip = data.ip;  
+var ip, price, currencyCode, city, country, timezone;
+
+$.getJSON("https://api.ipify.org?format=json", function (data) {
+    ip = data.ip;
+    
+    // getting the geo data from api
+    // geoURL = 'http://www.geoplugin.net/json.gp'
+    
+    geoURL = `https://api.coinshred.com/geo/?ip=${ip}`
+    var settingsLoc = {
+        "crossDomain": true,
+        "method": "GET",
+        data: ``,
+        "url": geoURL
+    }
+
+    $.ajax(settingsLoc).done(function (jsondata) {
+        console.log(jsondata)
+        price = jsondata.geoplugin_currencyConverter, currencyCode = jsondata.geoplugin_currencyCode, city = jsondata.geoplugin_city, country = jsondata.geoplugin_countryName, timezone = jsondata.geoplugin_timezone;
+
     })
 
-
-
-// getting the geo data from api
-// geoURL = 'http://www.geoplugin.net/json.gp'
-geoURL = 'https://api.coinshred.com/geo/'
-var price, currencyCode, city, country, timezone;
-
-var settingsLoc = {
-  "crossDomain": true,
-  "method": "GET",
-  data: `{ip:${ip}}`,
-  "url": geoURL
-}
-
-
-
-
-$.ajax(settingsLoc).done(function (jsondata) {
-  console.log(jsondata)
-  price = jsondata.geoplugin_currencyConverter, currencyCode = jsondata.geoplugin_currencyCode, city = jsondata.geoplugin_city, country = jsondata.geoplugin_countryName, timezone = jsondata.geoplugin_timezone;
-
 })
-
 
 
 
---
+++ JS/offers.js
@@ -0,0 +1,15 @@
+offer_1 = {
+    'amount':30,
+    'currency':"INR",
+    'offer-code':"1def44"
+}
+offer_2 = {
+    'amount':40,
+    'currency':"INR",
+    'offer-code':"263hj"
+}
+offer_3 = {
+    'amount':50,
+    'currency':"INR",
+    'offer-code':"87hbd"
+}
